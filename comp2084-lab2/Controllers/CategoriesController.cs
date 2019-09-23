d08c1478b1ad24010ac5308d49b3da746cd798a2
zdzulak <43354865+zdzulak@users.noreply.github.com>
2019-09-23 12:37:43 -0400

    first upload

---
+++ comp2084-lab2/Controllers/CategoriesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace comp2084_lab2.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        // GET: Categories
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Details(string category)
+        {
+
+            ViewBag.category = "You selected category: " + category;
+            return View();
+        }
+    }
+}
---
+++ comp2084-lab2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="comp2084_lab2.MvcApplication" Language="C#" %>
